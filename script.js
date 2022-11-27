0641edb36c2056f8b34e28ff7508f57cddef0782
Briana Grant <66385889+grantbriana@users.noreply.github.com>
2022-11-27 23:22:28 +0000

    Update

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 
  function colorChange() {
   var main1Box = document.getElementById('main1');
-  //main1Box.style.color = 'black';
-  main1Box.style.color = 'white';
-  //main1Box.style.backgroundColor = '#F3DAD8';
-   main1Box.style.backgroundColor = '#79A9D1';
-
+  main1Box.style.color = 'black';
+  main1Box.style.backgroundColor = '#e8e8e8';
 
   var main2Box = document.getElementById('main2');
-  //main2Box.style.color = 'black';
-  main2Box.style.color = 'white';
-  //main2Box.style.backgroundColor = '#F4C3C2';
-  main2Box.style.backgroundColor = "#36311F";
-
-
+  main2Box.style.color = 'black';
+  main2Box.style.backgroundColor = "#white";
 
   var footerBox = document.getElementById('footer');
-  footerBox.style.color = 'black';
-  footerBox.style.backgroundColor = '#D6D9CE';
+  footerBox.style.color = 'white';
+  footerBox.style.backgroundColor = 'black';
 
   var headerBox = document.getElementById('header');
-  //headerBox.style.backgroundColor = '#F1B5CB';
-   headerBox.style.backgroundColor = '#0C0C0C';
+  headerBox.style.backgroundColor = '#0C0C0C';
 
+  var navBar = document.getElementById('navBar');
+  navBar.style.backgroundColor = '#8c8c8c';
 }
 
 function chartOnLoad(){
@@ -47,6 +41,56 @@ function chartOnLoad(){
       }
     }
   });
+}
+
+function quiz(){
+  const btn = document.querySelector('#submit');
+const output = document.querySelector('#result');
+
+const radioButtons2 = document.querySelectorAll('input[name="pop_language"]');
+
+const radioButtons1 = document.querySelectorAll('input[name="prog_women"]');
+
+const radioButtons3 = document.querySelectorAll('input[name="self_taught"]');
+  
+
+
+btn.addEventListener("click", () => {
+            let answer2;
+            for (const radioButton of radioButtons2) {
+                if (radioButton.checked.value == "Javascript") {
+                    answer2 = "Correct!";
+                    break;
+                }
+              else{
+                answer2 = "Incorrect. The correct answer was: Javascript "
+              }
+
+              
+              let answer1;
+              for (const radioButton of radioButtons1) {
+                if (radioButton.checked.value == "True") {
+                    answer1 = "Correct!";
+                    break;
+                }
+              else{
+                answer1 = "Incorrect. The correct answer was: True"
+              }
+
+                
+                let answer3;
+                for (const radioButton of radioButtons3) {
+                if (radioButton.checked.value == "69%") {
+                    answer3 = "Correct!";
+                    break;
+                }
+              else{
+                answer3 = "Incorrect. The correct answer was: 69%"
+            }
+              }
+  }
+            
+
 }
 
 window.onload = chartOnLoad()
